1903eb2613b8f6f6ac54b17528060fa3cc5e411d
Octavio <tudaeva@gmail.com>
2021-08-06 14:19:46 -0700

    updated README.md with future features

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Instructions:
 1. Launch the server
 2. shorten a URL by sending a POST request to http:localhost:8080/cut, with {"url": "<Your URL>"} json body. It will return a code.
 3. Set the code as route and use it to visit the URL.
+
+Future Features:
+
+1. Install nodemon for dev debugging
+2. Connect to Reddis.io
+3. Implement validator.js library
+4. Switch Storage.data.links to an object to use Storage.data.links[link/code] instead of Storage.data.links.forEach(linkObject)
--- src/cutURL.js
+++ src/cutURL.js
@@ -1,4 +1,5 @@
 import { Storage } from "./data/Storage.js";
+import validator from 'validator';
 
 export const cutURL = (req, res) => {
   // To make sure URL is valid, we will use the validator regex
@@ -6,24 +7,24 @@ export const cutURL = (req, res) => {
 
   // check if the URL is valid, if not, send 400 response
   if (req.body.url === undefined || !urlRegex.test(req.body.url)) {
-    console.log('req.body.url: ', req.body.url);
+    // console.log('req.body.url: ', req.body.url);
     return res.status(400).send("Bad request");
+  } else {
+    // In case we successfully manage to validate the URL, we would need to generate a key/code for it, for this we will use replace method that will make a string of 5 hexadecimal characters.
+    const code =  "xxxxx".replace(/x/g, () =>
+      Math.floor(Math.random() * 16).toString(16)
+    );
+
+    Storage.data.links.push({
+      url: req.body.url,
+      code: code
+    });
+    Storage.write();
+
+    res.status(200).send({
+      code: code
+    });
   };
-
-  // In case we successfully manage to validate the URL, we would need to generate a key/code for it, for this we will use replace method that will make a string of 5 hexadecimal characters.
-  const code =  "xxxxx".replace(/x/g, () =>
-    Math.floor(Math.random() * 16).toString(16)
-  );
-
-  Storage.data.links.push({
-    url: req.body.url,
-    code: code
-  });
-  Storage.write();
-
-  res.status(200).send({
-    code: code
-  });
 };
 
 
