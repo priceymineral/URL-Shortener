11d17660f2baa43b2599ba0a645edb16c58f6fe5
Octavio <tudaeva@gmail.com>
2021-08-09 15:47:59 -0700

    switched to RegEx for improved URL validation in cutURL.js

--- src/cutURL.js
+++ src/cutURL.js
@@ -1,29 +1,41 @@
 import { Storage } from "./data/Storage.js";
-import validator from 'validator';
 
 export const cutURL = (req, res) => {
-  console.log(validator.isURL(req.body.url));
-  console.log(req.body.url);
-  if (validator.isURL(req.body.url)) {
+  const urlRegex = /^(https?:\/\/)?[\d\w]+\.[\w]+(\/.*)*/;
+  console.log("validator test:", req.body.url === undefined || !urlRegex.test(req.body.url));
+  console.log("request URL:", req.body.url);
+
+  if (req.body.url === undefined || !urlRegex.test(req.body.url)) {
     const code =  "xxxxx".replace(/x/g, () =>
       Math.floor(Math.random() * 16).toString(16)
     );
 
     Storage.data.links[code] = req.body.url; // add key value to links
     Storage.write(); // writes to storage.json
-
     res.status(200).send({
       code: code
     });
   } else {
-    return res.status(400).send("Bad request");
+    return res.status(400).send("Bad request, your URL is invalid");
   };
 };
 
-// URL VALIDATION USING REGEX:
-  // To make sure URL is valid, we will use the validator regex
-  // const urlRegex = /^(https?:\/\/)?[\d\w]+\.[\w]+(\/.*)*/;
+// URL VALIDATION USING validator.js:
+// import validator from 'validator';
 
-  // check if the URL is valid, if not, send 400 response
-  // if (req.body.url === undefined || !urlRegex.test(req.body.url)) {
-  //   return res.status(400).send("Bad request");
+// export const cutURL = (req, res) => {
+//   // console.log("validator test:", validator.isURL(req.body.url));
+//   // console.log("request URL:", req.body.url);
+//   if (validator.isURL(req.body.url)) {
+//     const code =  "xxxxx".replace(/x/g, () =>
+//       Math.floor(Math.random() * 16).toString(16)
+//     );
+//     Storage.data.links[code] = req.body.url; // add key value to links
+//     Storage.write(); // writes to storage.json
+//     res.status(200).send({
+//       code: code
+//     });
+//   } else {
+//     return res.status(400).send("Bad request, your URL is invalid");
+//   };
+// };
