23b08b827b86a6580ac2deb869d5c3ef7e786812
Octavio <tudaeva@gmail.com>
2021-08-18 18:28:45 -0700

    creating connection to mongodb

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Instructions:
 
 1. Launch the server
 2. shorten a URL by sending a POST request to http:localhost:8080/cut, with {"url": "<Your URL>"} json body. It will return a code.
-3. Set the code as route and use it to visit the URL.
+3. Set the code as route to visit the URL.
 
 Future Features:
 
--- src/cutURL.js
+++ src/cutURL.js
@@ -1,35 +1,39 @@
 import { Storage } from "./data/Storage.js";
 
-export const cutURL = (req, res) => {
-  const urlRegex = /^(https?:\/\/)?[\d\w]+\.[\w]+(\/.*)*/;
-  console.log("validator test:", req.body.url === undefined || !urlRegex.test(req.body.url));
-  console.log("request URL:", req.body.url);
+// URL VALIDATION USING validator.js:
+import validator from 'validator';
 
-  if (req.body.url === undefined || !urlRegex.test(req.body.url)) {
+export const cutURL = (req, res) => {
+  console.log("validator test:", validator.isURL(req.body.url));
+  console.log("request URL:", req.body.url);
+  if (validator.isURL(req.body.url)) {
     const code =  "xxxxx".replace(/x/g, () =>
       Math.floor(Math.random() * 16).toString(16)
     );
-
     Storage.data.links[code] = req.body.url; // add key value to links
     Storage.write(); // writes to storage.json
     res.status(200).send({
       code: code
     });
+    console.log("Success! Saved the URL.")
   } else {
     return res.status(400).send("Bad request, your URL is invalid");
   };
 };
 
-// URL VALIDATION USING validator.js:
-// import validator from 'validator';
 
+
+// // Validation using RegEx
 // export const cutURL = (req, res) => {
-//   // console.log("validator test:", validator.isURL(req.body.url));
-//   // console.log("request URL:", req.body.url);
-//   if (validator.isURL(req.body.url)) {
+//   const urlRegex = /^(https?:\/\/)?[\d\w]+\.[\w]+(\/.*)*/;
+//   console.log("validator test:", req.body.url === undefined || !urlRegex.test(req.body.url));
+//   console.log("request URL:", req.body.url);
+
+//   if (req.body.url === undefined || !urlRegex.test(req.body.url)) {
 //     const code =  "xxxxx".replace(/x/g, () =>
 //       Math.floor(Math.random() * 16).toString(16)
 //     );
+
 //     Storage.data.links[code] = req.body.url; // add key value to links
 //     Storage.write(); // writes to storage.json
 //     res.status(200).send({
