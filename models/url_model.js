4bec7a59fc88a26282c8b4d0ea2954ccd682847e
Octavio <tudaeva@gmail.com>
2021-08-18 18:51:18 -0700

    url model exported and imported

--- models/url_model.js
+++ models/url_model.js
@@ -1,10 +1,15 @@
 //Require Mongoose
-var mongoose = require('mongoose');
-
+import mongoose from 'mongoose';
 //Define a schema
 var Schema = mongoose.Schema;
 
-var SomeModelSchema = new Schema({
+var URLSchema = new Schema({
   url: String,
   code: Number
 });
+
+// Compile model from schema
+let URLModel = mongoose.model('URL', URLSchema );
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export#using_named_exports
+export { URLModel };
--- src/cutURL.js
+++ src/cutURL.js
@@ -1,5 +1,5 @@
 import { Storage } from "./data/Storage.js";
-
+import { URLModel } from "../models/url_model.js";
 // URL VALIDATION USING validator.js:
 import validator from 'validator';
 
@@ -7,19 +7,39 @@ export const cutURL = (req, res) => {
   console.log("validator test:", validator.isURL(req.body.url));
   console.log("request URL:", req.body.url);
   if (validator.isURL(req.body.url)) {
+    // Create code
     const code =  "xxxxx".replace(/x/g, () =>
       Math.floor(Math.random() * 16).toString(16)
     );
-    Storage.data.links[code] = req.body.url; // add key value to links
-    Storage.write(); // writes to storage.json
-    res.status(200).send({
-      code: code
-    });
+
+    // Create model using url_models.js
+    let urlToSave = new URLModel({url: req.body.url, code: code});
+
     console.log("Success! Saved the URL.")
   } else {
     return res.status(400).send("Bad request, your URL is invalid");
   };
 };
+// // URL VALIDATION USING validator.js:
+// import validator from 'validator';
+
+// export const cutURL = (req, res) => {
+//   console.log("validator test:", validator.isURL(req.body.url));
+//   console.log("request URL:", req.body.url);
+//   if (validator.isURL(req.body.url)) {
+//     const code =  "xxxxx".replace(/x/g, () =>
+//       Math.floor(Math.random() * 16).toString(16)
+//     );
+//     Storage.data.links[code] = req.body.url; // add key value to links
+//     Storage.write(); // writes to storage.json
+//     res.status(200).send({
+//       code: code
+//     });
+//     console.log("Success! Saved the URL.")
+//   } else {
+//     return res.status(400).send("Bad request, your URL is invalid");
+//   };
+// };
 
 
 
