db1b3c3a4a55b91f47cfdec028b389d9039a8d39
Octavio <tudaeva@gmail.com>
2021-09-07 15:48:20 -0700

    cleaned up cutURL function

--- app/controllers/cut_controller.js
+++ app/controllers/cut_controller.js
@@ -1,9 +1,8 @@
-// URL VALIDATION USING validator.js:
-import validator from 'validator';
-// import { client } from '../../connection.js';
+// import validator from 'validator';
 import { UrlModel } from '../models/url_model.js';
 
 function validURL(str) {
+  // https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
   var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
@@ -14,50 +13,43 @@ function validURL(str) {
 }
 // potential validation => https://dev.to/calvinpak/simple-url-validation-with-javascript-4oj5
 async function cutURL(req, res) {
-  const urlRegex = /^(https?:\/\/)?[\d\w]+\.[\w]+(\/.*)*/;
-  console.log('validator.js result:', validator.isURL(req.body.url));
-  console.log("Short RegEx validation:", req.body.url === undefined || !urlRegex.test(req.body.url));
-  console.log("request URL:", req.body.url);
-
   if (validURL(req.body.url)) {
     const code =  "xxxxx".replace(/x/g, () =>
       Math.floor(Math.random() * 16).toString(16)
     );
-
     // call to save the url and code to DB
-    async function main() {
-      try {
-        const instance = new UrlModel({
-          url: req.body.url,
-          code: code
-        })
+    try {
+      const instance = new UrlModel({
+        url: req.body.url,
+        code: code
+      })
 
-        await instance.save(e => {
-          if (!e) {
-            console.log('Successfully saved url!');
-            res.status(200).send({
-              code: code
-            })
-          } else {
-            console.log('something wrong:', e);
-          }
-        })
+      await instance.save(e => {
+        if (!e) {
+          console.log('Successfully saved url!');
+          res.status(200).send({
+            code: code
+          })
 
-      } catch (e) {
-        console.error(e);
-      }
-    }
+        } else {
+          console.log('something wrong:', e);
+        }
+      })
 
-    main().catch(console.error);
+    } catch (e) {console.error(e)};
 
   } else {
     return res.status(400).send("Bad request, your URL is invalid");
   };
 };
 
-
-
 export { cutURL }
+
+// TESTING VALIDATION
+// const urlRegex = /^(https?:\/\/)?[\d\w]+\.[\w]+(\/.*)*/;
+// console.log('validator.js result:', validator.isURL(req.body.url));
+// console.log("Short RegEx validation:", req.body.url === undefined || !urlRegex.test(req.body.url));
+// console.log("request URL:", req.body.url);
 
 // // VALIDATION USING RegEx
 // export const cutURL = (req, res) => {
